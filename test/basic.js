a700459f8b6ee467ab4eeb17c951e6ec2d11a138
Peter Krautzberger <p.krautzberger@gmail.com>
2020-10-02 22:15:19 +0200

    docs: adjust basic.js for doc purposes
    Applies sre-to-tree to the SVG node and mentions issue #6.

--- test/basic.js
+++ test/basic.js
@@ -5,9 +5,10 @@ const tex2svg = require('./tex2svg');
 test('a first few tests', (t) => {
   t.plan(3);
   const out = tex2svg('f(x) = y');
-  sre2tree(out);
+  const svg = out.firstElementChild;
+  sre2tree(svg); // NOTE with SVG output, rewrite the SVG node cf. krautzource/sre-to-tree#6
 
-  t.equal(out.getAttribute('role'), 'tree', 'Root gets role=tree');
-  t.ok(out.getAttribute('aria-owns').split(' ').length > 0 , 'Root gets non-emtpy aria-owns');
-  t.ok(out.getAttribute('aria-label'), 'Root gets non-emtpy aria-owns');
+  t.equal(svg.getAttribute('role'), 'tree', 'Root gets role=tree');
+  t.ok(svg.getAttribute('aria-owns').split(' ').length > 0 , 'Root gets non-emtpy aria-owns');
+  t.ok(svg.getAttribute('aria-label'), 'Root gets non-emtpy aria-owns');
 });
