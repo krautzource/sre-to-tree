ed5e74b54adcebff34c5109314898eeb3285370b
Peter Krautzberger <p.krautzberger@gmail.com>
2021-01-29 17:09:36 +0100

    test(error): remove broken test
    Closes #12

--- test/error.js
+++ test/error.js
@@ -4,13 +4,12 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 test('failure tests', (t) => {
-  t.plan(3);
-  t.equal(sre2tree('hello'), 'hello', 'Noop when input is not an element');
+  t.plan(2);
   const dom = new JSDOM(
     `<!DOCTYPE html><p>Hello world</p><div> <span data-semantic-speech data-semantic-owns="2"></span</div>`
   );
   const p = dom.window.document.querySelector('p');
   t.equal(sre2tree(p), p, 'Noop when no SRE markup');
   const div = dom.window.document.querySelector('div');
-  t.equal(sre2tree(div), div, 'Noop when element fails');
+  t.equal(sre2tree(div), div, 'Graceful failure with unexpected markup');
 });
