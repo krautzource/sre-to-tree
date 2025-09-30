fe14d721608b09135997095bdcc4c03f185e808f
Peter Krautzberger <p.krautzberger@gmail.com>
2025-09-30 13:03:18 +0200

    refactor(anchor.js): prep for more tests
    Avoids confusing git diff down the line.

--- test/anchors.js
+++ test/anchors.js
@@ -4,10 +4,10 @@ import { tex2svg } from './tex2svg.js';
 
 tape('anchors', async (t) => {
   t.plan(5);
-  const out = await tex2svg('a  = \\href{//example.com}{link}');
-  const svg = out.firstElementChild;
+  let out = await tex2svg('a  = \\href{//example.com}{link}');
+  let svg = out.firstElementChild;
   sre2tree(svg);
-  const theLink = svg.querySelector('a[href]');
+  let theLink = svg.querySelector('a[href]');
   t.equal(theLink.getAttribute('role'), 'treeitem' , 'Anchor gets role from "real" SRE node');
   t.equal(theLink.getAttribute('data-owns-id'), '9', 'Anchor gets data-owns-id from "real" SRE node');
   t.notOk(theLink.querySelector('[data-owns-id="9"]'), 'Anchor descendents do not have the same data-owns-id, i.e., removed from "real" SRE node.')
